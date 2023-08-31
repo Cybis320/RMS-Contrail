7b51d5ad61bc8086fd6cf2b447c3fc5dc69218cc
Cybis320 <luc.busquin@gmail.com>
2023-08-30 18:20:09 -0700

    Fix typo

--- RMS/BufferedCapture.py
+++ RMS/BufferedCapture.py
@@ -380,7 +380,7 @@ class BufferedCapture(Process):
 
                         # Draw text to image
                         font = cv2.FONT_HERSHEY_SIMPLEX
-                        text = self.config.stationID + " " + date_string + " " + millis + " UTC"
+                        text = f"{self.config.stationID} {date_string} {millis} UTC"
                         cv2.putText(frame, text, (10, maxpixel.shape[0] - 6), font, 0.4, (255, 255, 255), 1, \
                             cv2.LINE_AA)
 
