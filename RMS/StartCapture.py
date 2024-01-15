bddc3676e8f853a60e326a54fdc6142f77ef40af
Cybis320 <luc.busquin@gmail.com>
2024-01-15 16:10:43 -0700

    Test 2 cores

--- RMS/StartCapture.py
+++ RMS/StartCapture.py
@@ -356,7 +356,7 @@ def runCapture(config, duration=None, video_file=None, nodetect=False, detect_en
             pass
 
         # Initialize the detector
-        detector = QueuedPool(detectStarsAndMeteors, cores=1, log=log, delay_start=delay_detection, \
+        detector = QueuedPool(detectStarsAndMeteors, cores=2, log=log, delay_start=delay_detection, \
             backup_dir=night_data_dir, input_queue_maxsize=None)
         detector.startPool()
 
